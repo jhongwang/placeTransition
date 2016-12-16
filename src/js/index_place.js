d4aa068903bd510afc579c1b40db63ce9c992080
v_jhongwang(王佳虹) <v_jhongwang@tencent.com>
2016-12-16 17:58:03 +0800

    模拟数据功能完成 还差图标随地图缩放的功能

--- src/js/index_place.js
+++ src/js/index_place.js
@@ -31,8 +31,15 @@ $('#poi_search').click(function(){
       }
      var lat = pos[1];
      var lng = pos[0];
+     cur_lng = lng;
+     cur_lat = lat;
     // poi_list(lng,lat,radius_);
    }
+})
+$('#time_select').change(function(){
+  data_cur={};
+  jQuery.extend(true,data_cur,data_get);
+  time_selected_show();
 })
 $('#search_lng_lat').click(function(){
    $(this).select();
@@ -44,14 +51,10 @@ $('.input_btn').mousedown(function(){
 $('.input_btn').mouseup(function(){
   $(this).css('background','#2d9cff');
 })
+
 //checkbox单选或者多选点击事件
 $('input[name=input_checkbox]').click(function(){
-  checkbox_selected=[];
-   $('input[name=input_checkbox]').each(function(e){
-     if($(this).context.checked){
-        checkbox_selected.push($(this).context.value);
-     }
-   })
+  checked_fun()
    console.log(checkbox_selected)
 })
 //搜索自动补全结果展示和点击事件
@@ -64,7 +67,10 @@ $('#ui-id-1 li').click(function(){
    var poi=$(this).find('em').html();
    var lat=poi.split(',')[0];
    var lng=poi.split(',')[1];
-   //poi_list(lng,lat,radius_);
+   cur_lng = lng;
+   cur_lat = lat;
+   cur_md =$(this).attr('class').split(' ')[0];
+   poi_list();
 })
 $('#ui-id-1 li').mouseover(function(){
   $(this).addClass('autoli_cur').siblings().removeClass('autoli_cur');
@@ -136,23 +142,40 @@ jQuery.ajaxSetup({
 });
 var checkbox_selected;//表示checkbox选择了哪些 是个数组
 var cur_city = '北京';//表示当前选中城市 暂时只有北京
-
-
+var start_busi;//当前筛选出来的起点 商圈
+var start_poi;//当前筛选出来的起点 POI
+var end_busi;//当前筛选出来的终点 商圈
+var end_poi;//当前筛选出来的终点 POI
+var cur_md = 3606955108245080331;//当前选择点的md
+var cur_lng = 116.31617;//当前选择点的lng
+var cur_lat = 40.04194;//当前选择点的lat
+var data_get;//获取ajax获取下来的数据 这个只有展示不可给改
+var data_cur={};//把当前的从后台获取的数据放在这里 可以更改
+jQuery.extend(true,data_cur,data_get);
 
 function initPage(lng,lat) {
       MapHelper.install(document.getElementById('root'), {
         lat:lat,
         lng:lng,
         onPoiChange: function(lng_, lat_) {
-          p=0;
-          var radius=$$('radius').value;
           MapHelper.setMarkerPostion(lat_, lng_);
           $('#search_lng_lat').val('中关村');
+
          // ajax_poilist.abort();
           //poi_list(lng_,lat_,raidus_);
          }
       });
+      poi_list();//初始化
 
+}
+function checked_fun(){
+  checkbox_selected=[];
+   $('input[name=input_checkbox]').each(function(e){
+     if($(this).context.checked){
+        checkbox_selected.push($(this).context.value);
+     }
+   })
+  time_selected_show();
 }
 function cur_city_code(key){
   $.ajax({
@@ -199,7 +222,7 @@ function cur_city_get(lat,lng){
               cur_code=Number(cur_code)
           }
           //cur_code=String(cur_code).
-          $('#city_login').val(cur_city);
+          //$('#city_login').val(cur_city);
          //console.log(cur_city)
          //console.log(cur_code)
         }
@@ -214,27 +237,158 @@ function cur_city_get(lat,lng){
       }
   })  
 }
-function draw_poi_daoguan(arr,show){
-  MapHelper.setMarkers_poi(arr,show);
-}
-function draw_poi_bujizhan(arr,show){
-  MapHelper.setMarkers_poi_radio(arr,show);
-}
+
 function $$(id){
   return document.getElementById(id);
 }
 
-
-
 function clearDitu(){
-    MapHelper.cleanInfo();
-    MapHelper.cleanMarker();
-    MapHelper.cleanPoi();
-    MapHelper.cleanRect();
-    MapHelper.cleanInfo();
     MapHelper.cleanMarkers_poi();
     MapHelper.cleanMarkers_poi_radio();
-    MapHelper.cleanMarkers_poi_guai();
+}
+function clearTable(){
+    $('#table_start tbody').html('');
+    $('#table_end tbody').html('');
+}
+
+function draw_data(){
+    clearDitu();
+    start_busi = [];
+    start_poi = [];
+    end_busi = [];
+    end_poi = [];
+    //商圈 POI 起点 终点
+    if(checkbox_selected.indexOf('起点') != -1){
+       if(checkbox_selected.indexOf('商圈') != -1){
+          var arr = data_cur.start_info.busi_list;
+          for(var i =0;i< arr.length; i++){
+             start_busi.push(arr[i]);
+          }
+       }
+       if(checkbox_selected.indexOf('POI') != -1){
+          var arr = data_cur.start_info.name_list;
+          for(var i =0;i< arr.length; i++){
+             start_poi.push(arr[i]);
+          }
+       }
+    }
+    if(checkbox_selected.indexOf('终点') != -1){
+       if(checkbox_selected.indexOf('商圈') != -1){
+          var arr = data_cur.end_info.busi_list;
+          for(var i =0;i< arr.length; i++){
+             end_busi.push(arr[i]);
+          }
+       }
+       if(checkbox_selected.indexOf('POI') != -1){
+           var arr = data_cur.end_info.name_list;
+          for(var i =0;i< arr.length; i++){
+             end_poi.push(arr[i]);
+          }
+       }
+    }
+  draw_marker();
+}
+function draw_data_other_time(target){
+    clearDitu();
+    start_busi = [];
+    start_poi = [];
+    end_busi = [];
+    end_poi = [];
+    //商圈 POI 起点 终点
+    if(checkbox_selected.indexOf('起点') != -1){
+       if(checkbox_selected.indexOf('商圈') != -1){
+          var arr = data_cur.start_info.busi_list;
+          for(var i =0;i< arr.length; i++){
+            if(arr[i].time_count == target){
+             start_busi.push(arr[i]);
+            }
+          }
+       }
+       if(checkbox_selected.indexOf('POI') != -1){
+          var arr = data_cur.start_info.name_list;
+          for(var i =0;i< arr.length; i++){
+             if(arr[i].time_count == target){
+                start_poi.push(arr[i]);
+             }
+             
+          }
+       }
+    }
+    if(checkbox_selected.indexOf('终点') != -1){
+       if(checkbox_selected.indexOf('商圈') != -1){
+          var arr = data_cur.end_info.busi_list;
+          for(var i =0;i< arr.length; i++){
+             if(arr[i].time_count == target){
+              end_busi.push(arr[i]);
+             }
+             
+          }
+       }
+       if(checkbox_selected.indexOf('POI') != -1){
+           var arr = data_cur.end_info.name_list;
+          for(var i =0;i< arr.length; i++){
+             if(arr[i].time_count == target){
+               end_poi.push(arr[i]);
+             }
+            
+          }
+       }
+    }
+  draw_marker();
+}
+function draw_marker(){
+  //console.log(start_busi)
+  //console.log(start_poi)
+  //console.log(end_busi)
+  //console.log(end_poi)
+  MapHelper.setMarkers_poi(start_busi,'商圈');
+  MapHelper.setMarkers_poi(start_poi,'POI');
+  MapHelper.setMarkers_poi_radio(end_busi,'商圈');
+  MapHelper.setMarkers_poi_radio(end_poi,'POI');
+  creatTable_start(start_busi,start_poi);
+  creatTable_end(end_busi,end_poi);
+}
+function creatTable_start(arr_busi,arr_poi){
+  $('#table_start tbody').html('');
+  var iHtml = '';
+  for(var i = 0; i<arr_busi.length; i++){
+     iHtml += '<tr>';
+     iHtml += '<td>'+arr_busi[i].class_code+'</td>';
+     iHtml += '<td>'+arr_busi[i].name+'</td>';
+     iHtml += '<td>'+arr_busi[i].time_count+'</td>';
+  }
+  for(var i = 0; i<arr_poi.length; i++){
+     iHtml += '<tr>';
+     iHtml += '<td>'+arr_poi[i].class_code+'</td>';
+     iHtml += '<td>'+arr_poi[i].name+'</td>';
+     iHtml += '<td>'+arr_poi[i].time_count+'</td>';
+  }
+  $('#table_start tbody').html(iHtml);
+}
+function creatTable_end(arr_busi,arr_poi){
+  $('#table_end tbody').html('');
+  var iHtml = '';
+  for(var i = 0; i<arr_busi.length; i++){
+     iHtml += '<tr>';
+     iHtml += '<td>'+arr_busi[i].class_code+'</td>';
+     iHtml += '<td>'+arr_busi[i].name+'</td>';
+     iHtml += '<td>'+arr_busi[i].time_count+'</td>';
+  }
+  for(var i = 0; i<arr_poi.length; i++){
+     iHtml += '<tr>';
+     iHtml += '<td>'+arr_poi[i].class_code+'</td>';
+     iHtml += '<td>'+arr_poi[i].name+'</td>';
+     iHtml += '<td>'+arr_poi[i].time_count+'</td>';
+  }
+  $('#table_end tbody').html(iHtml);
+}
+function time_selected_show(){
+  var target = $('#time_select').val();
+  if(target == 1){
+     draw_data();
+  }else{
+     draw_data_other_time(target)
+  }
 }
 function GetQueryString(name) {
    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)","i");
@@ -254,76 +408,48 @@ function GetRequest() {
     return theRequest; 
 }
 
-function poi_list(lng,lat,radius){
+function poi_list(){
+    $('#ajax_tip').css('display','block');
+    setTimeout(function(){
+      $('#ajax_tip').css('display','none');
+    },1000);
     clearDitu();
-    arr_bujizhan=[];
-    arr_daoguan=[];
-    other_arr=[];
-    $('#ajax_tip').css('display','none');
-    cur_city_get(lat,lng);
-    var d={};
-    var b={};
-    var g={};
-    var version=$('#banben_sel').val();
-    //console.log(version)
-    version=version.replace(/\s/g, '');
-   // console.log(version)
-    //http://10.173.142.164:8080/pokemon/pokemon_whole.php?point_x=114.1351032&point_y=22.372129267&radius=500&ref=timi_version&output_type=jsonp&version=20160618210508&cb=test
-    //&only_frag=1 这个是限制道馆补给数据
-$.ajax({
+    clearTable();
+    MapHelper.setCenter(cur_lat,cur_lng);
+    MapHelper.setMarkerPostion(cur_lat,cur_lng);
+    //data_get=[];
+    //$('#ajax_tip').css('display','none');
+
+   data_get={end_info:{"busi_list": [{"md": "5176743892522660139", "name": "东关", "point_x": "116.643755", "point_y": "40.314403", "class_code": "261612", "time": "2016-08-01 01:25:18", "time_count": "2"}], "id_type": "9997096193932611952|start", "name_list": [{"md": "9997096193932611952", "name": "东关(一区)", "point_x": "116.642755", "point_y": "40.314401", "class_code": "281010", "time": "2016-08-01 01:25:18", "time_count": "6"}]},start_info:{"busi_list": [{"md": "3606955108245080331", "name": "清河", "point_x": "116.346", "point_y": "40.0285", "class_code": "261612", "time": "2016-08-01 00:52:56", "time_count": "5"}], "id_type": "9999716292397959034|start", "name_list": [{"md": "13961079153525193356", "name": "清缘里", "point_x": "116.356609", "point_y": "40.029031", "class_code": "281010", "time": "2016-08-01 00:52:56", "time_count": "4"}, {"md": "9999716292397959034", "name": "清缘里(中区)", "point_x": "116.355609", "point_y": "40.029033", "class_code": "281010", "time": "2016-08-01 00:52:56", "time_count": "3"}]}};
+    data_cur={};
+    jQuery.extend(true,data_cur,data_get);
+    checked_fun();
+/*$.ajax({
       type:"get",
-      //url:"http://10.173.142.164:8080/pokemon/pokemon_whole.php?point_x="+lng+"&point_y="+lat+"&radius="+radius+"&ref=timi_version&version="+version+"&output_type=jsonp&time="+(new Date()).getTime()+"",
-	  url:"http://10.173.142.164:8080/poke_timi_200916/php/get_frag_info_timi.php?point_x="+lng+"&point_y="+lat+"&radius="+radius+"&key="+key+"&output_type=jsonp&cb=ajaxpoilist&time="+(new Date()).getTime()+""+"&ref=timi_version",
+	  url:"http://10.173.142.164:8080/poke_timi_200916/php/get_frag_info_timi.php?md="+cur_md+"&output_type=jsonp&time="+(new Date()).getTime()+"",
       dataType:"jsonp",
       jsonp:"cb",
       jsonpCallback:"test",
       success:function(data){
-		  if(data.key&& data.key!=-1){
-        $('#ajax_before_tip').css('display','none');
-        if(data.gyms_list&&data.pokestops_list&&data.frag_list){
-           jQuery.extend(true,d,data);
-           jQuery.extend(true,b,data);
-           jQuery.extend(true,g,data);
-           MapHelper.setCenter(lat,lng);
-           MapHelper.setMarker(lat,lng);
-           for(var i=0;i<tuli_title.length;i++){
-            tuli_title[i].num=0;
-           }
-           arr_bujizhan=b.pokestops_list;
-           arr_daoguan=d.gyms_list;
-           arr_guai=g.spawns_list;
-           ditubiaoji_d=d.gyms_list;
-           ditubiaoji_b=b.pokestops_list;
-           ditubiaoji_g=g.spawns_list;
-           other_arr=d.frag_list;
-           otherInfo(other_arr);
-           drawRs(other_arr);
-           zuixin_daoguan_arr=d.gyms_list;
-           zuixin_buji_arr=b.pokestops_list;
-           zuixin_guai_arr=g.spawns_list;
-           poi_list_show(zuixin_daoguan_arr,zuixin_buji_arr,zuixin_guai_arr);
+		    if(data){
+        
         }
         else{
            $('#ajax_tip').css('display','block');
            MapHelper.cleanPoi();
         }
-		  }
-		else
-		{
-			poi_list_wuquan(lng,lat,radius);
-		}
-		  
+
       },
       error:function(ys){
-         console.log('fail_poi_latlng')
+         console.log('fail_poi_list')
       },
       beforeSend:function(){
         $('#ajax_before_tip').css('display','block');
       },
       complete:function(){
-        window.location.hash='point_x='+lng+'&point_y='+lat+'&radius='+$$('radius').value+'';
+        window.location.hash='md='+cur_md+'';
       }
-  })
+  })*/
 }
 
 
@@ -347,8 +473,12 @@ function poi_search_wenzi(poi_w){
         jsonp:"cb",
         jsonpCallback:"search_result",
         success:function(json){
-        if(json){
-           console.log(json)
+        if(json&&json.data){
+           cur_md = json.data[0].id;
+           cur_lng = json.data[0].location.lng;
+           cur_lat = json.data[0].location.lat;
+           poi_list();
+           //这里通过md去获取结果 通过经纬度改变地图中心点
         }else{
           alert('抱歉,搜索结果为空！')
         }
@@ -363,15 +493,7 @@ function poi_search_wenzi(poi_w){
       }
    });
 }
-function imgShow(id){
-  var content2 = '<img width="300" height="500" src="http://svpoi.wsd.com/poi_image/shenzhen/'+id+'_gl.jpg" onerror="nofind();"/>';
-  TINY.box.show(content2,0,0,0,1)
-}
-function nofind(){
-    var img=event.srcElement;
-    img.src='images/nofind.jpg';
-    img.onerror=null;
-}
+
 
 
 /*自动补全js*/
@@ -507,4 +629,9 @@ AutoComplete.prototype={
         window.onresize=Bind(this,function(){this.init();});
     }
 }
-1
+
+
+
+console.log(data_get)
+console.log(data_cur)
+
